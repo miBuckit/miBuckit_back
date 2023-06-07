57305caf7dc8a76afd5450d2671122235c88ce69
eundoo <jaksimfive@naver.com>
2023-06-07 19:33:40 +0900

    first commit

---
+++ src/main/java/com/olTeam/mibuckit/MibuckitApplication.java
@@ -0,0 +1,13 @@
+package com.olTeam.mibuckit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibuckitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibuckitApplication.class, args);
+	}
+
+}
