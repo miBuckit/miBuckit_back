bfa730f1539fc0d5be3a339957da54560be5820c
sjlee <happybin2013@google.com>
2023-06-11 22:50:32 +0900

    add

---
+++ src/main/java/com/olTeam/mibuckit/MibuckitApplication.java
@@ -0,0 +1,25 @@
+package com.olTeam.mibuckit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MibuckitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibuckitApplication.class, args);
+	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
---
+++ src/test/java/com/olTeam/mibuckit/MibuckitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olTeam.mibuckit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibuckitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
