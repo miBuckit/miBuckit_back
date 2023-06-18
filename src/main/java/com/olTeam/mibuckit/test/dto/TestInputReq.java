a71efd66d8499d6b55e32d9fd884ed72cabd9cc4
sjlee <happybin2013@google.com>
2023-06-18 17:26:46 +0900

    add get post sample

--- src/main/java/com/olTeam/mibuckit/test/dto/TestInputReq.java
+++ src/main/java/com/olTeam/mibuckit/test/dto/TestInputReq.java
@@ -7,7 +7,7 @@ import lombok.EqualsAndHashCode;
 
 @Data
 public class TestInputReq extends Audit {
-	private int testId;
+	private String testId;
 	private String testNm;
 	private String testPw;
 }
